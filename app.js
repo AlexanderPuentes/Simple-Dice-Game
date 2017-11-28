6713acb14bcae3f4c1f7059747d57d63d01babd4
AlexanderPuentes <alexander.puen91@gmail.com>
2017-11-28 13:18:55 -0500

    dice click function

--- app.js
+++ app.js
@@ -15,14 +15,9 @@ scores = [0,0];
 roundScores = 0;
 activePlayer = 0;
 
-console.log(dice);
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+// var x = document.querySelector('#score-0').textContent;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -31,6 +26,9 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   var dice = Math.floor(Math.random() * 6) + 1;
 
   // 2. display result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
 
   // 3. update round score IF the rolled number was NOT a 1
 
