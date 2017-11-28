ad4fcca6c001255136da0d447589098057e14689
AlexanderPuentes <alexander.puen91@gmail.com>
2017-11-28 13:10:28 -0500

    adding click function to roll dice

--- app.js
+++ app.js
@@ -9,13 +9,12 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer, dice;
+var scores, roundScores, activePlayer;
 
 scores = [0,0];
 roundScores = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
 console.log(dice);
 
 document.querySelector('#current-' + activePlayer).textContent = dice;
@@ -26,3 +25,14 @@ var x = document.querySelector('#score-0').textContent;
 console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // 1. roll a random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. display result
+
+  // 3. update round score IF the rolled number was NOT a 1
+
+
+});
